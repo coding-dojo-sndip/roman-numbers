af2b9b0477f37b74ae7187b3c97583da1666bf4f
Romain Warnan <romain.warnan@insee.fr>
2018-03-30 12:58:32 +0200

    Suppression du code pour le coding-dojo

--- src/main/java/fr/insee/roman/InputException.java
+++
@@ -1,9 +0,0 @@
-package fr.insee.roman;
-
-@SuppressWarnings("serial")
-public class InputException extends RuntimeException {
-
-	public InputException(Integer integer) {
-		super(String.format("Number %d is not a valid input high, valid range is {1; 3999}", integer));
-	}
-}
